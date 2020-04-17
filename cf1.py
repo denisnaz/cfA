32344484474cf796732e85e5f2d94c66ec7d7a8e
Denis Nazarov <den_nazar-99@mail.ru>
2020-04-17 16:57:04 +0600

    2

--- cf1.py
+++ cf1.py
@@ -7,15 +7,10 @@ data = list(map(int, nums.split(' ')))
 parts_list = [[] for part in range (parts)]
 parts_num = 0
 for i_classes in range (classes):
-    i_objects = 0
-    for i in range(objects):
+    for i_objects in range(objects):
         if data[i_objects] == i_classes + 1:
             parts_list[parts_num].append(i_objects+1)
-            del data[i_objects]
-            objects = objects - 1
             parts_num = (parts_num + 1) % parts
-        else:
-            i_objects+=1
 
 for part in parts_list:
     print(len(part), *part)
